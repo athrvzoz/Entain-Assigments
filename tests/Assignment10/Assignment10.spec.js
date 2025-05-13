ad5368ec831214d64353f27c330cb893612b7015
Atharva Vaibhav Deshmukh <atharva.deshmukh@zensar.com>
2025-05-13 16:53:17 +0530

    Allure

--- tests/Assignment10/Assignment10.spec.js
+++ tests/Assignment10/Assignment10.spec.js
@@ -4,7 +4,7 @@ const path = require('path');
 test.describe('Assignment 10', () => {
     test('Upload a file', async ({ page }) => {
         // const filePath = 'C:/Users/AD115408/Pictures/Screenshots/Screenshot (1).png';
-        await page.goto('http://127.0.0.1:5500/file-upload-demo.html');
+        await page.goto('http://127.0.0.1:5500/tests/Assignment10/file-upload-demo.html');
         const fileChooserPromise = page.waitForEvent('filechooser');
         await page.locator('#fileInput').click();
         const fileChooser = await fileChooserPromise;
